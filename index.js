68f98115a7b03a93c6efcb735e0b2e4bea7fed3e
ittp <61617660+ittp@users.noreply.github.com>
2022-04-15 02:37:54 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const ssh = require('ssh2')
+console.log("main loaded");
+
+const server = require("koa2");
+const axios = require("axios");
+
+const app = server
+console.log(app);
+
+
+const hub = axios.create({
+  baseURL: 'http://hub.ittps.ru:8888'
+})
+
+const is = axios.create({
+  baseURL: 'http://is.ittps.ru:8888'
+})
+
+const tp = axios.create({
+  baseURL: 'http://tp.ittps.ru:8888'
+})
+
+const data = axios.create({
+  baseURL: 'http://tp.ittps.ru:8888'
+})
+
+console.log( data.get('/') )
+
+// module.exports = {};
